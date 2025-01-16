1955e6cada2ed7fa891234da5241c8cc3a29ae29
sauravdani <sauravdani7@gmail.com>
2025-01-17 00:27:44 +0530

    made files

---
+++ Locations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Locations
+{
+private:
+    /* data */
+public:
+    Locations(/* args */);
+    ~Locations();
+};
+
+Locations::Locations(/* args */)
+{
+}
+
+Locations::~Locations()
+{
+}
+
+int main()
+{
+}
